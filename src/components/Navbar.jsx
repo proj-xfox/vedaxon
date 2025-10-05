0ac59d51777bafe7e556955f59963bd7878f8f77
Sachin Thawari <sachin.thawari101@gmail.com>
2025-10-05 10:37:54 +0530

    Modify Nav bar CNAME added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center fixed w-full top-0 shadow-md z-10">
-      <h1 className="text-xl font-bold">Sachin Thawari</h1>
+      <h1 className="text-xl font-bold">Vedaxon</h1>
       <ul className="flex space-x-6">
         <li><a href="#skills" className="hover:text-yellow-400">Skills</a></li>
         <li><a href="#projects" className="hover:text-yellow-400">Projects</a></li>
